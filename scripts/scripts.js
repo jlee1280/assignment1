b7e4f9717620a1f884e8eb7e1f0cb72165084a9f
Jimmy <jlee1280@my.bcit.ca>
2021-04-07 22:23:08 -0700

    note to self

---
+++ scripts/scripts.js
@@ -0,0 +1,26 @@
+// Notes:
+
+
+{/* <script type="text/javascript">
+  document.addEventListener("DOMContentLoaded", function(event) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  });
+</script> */}
+
+
+{/* <script>
+function myFunction() {
+  document.getElementById("myForm").reset();
+}
+</script> */}
+
+
+{/* <script>
+  var form = document.querySelector("form");
+  form.addEventListener("submit", function(event) {
+    console.log("Saving value", form.elements.value.value);
+    event.preventDefault();
+  });
+</script> */}
+
+
